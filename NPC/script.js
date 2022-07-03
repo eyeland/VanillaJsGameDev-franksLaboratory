dbc34475c6dd7a143776813e2f857da65f80189f
eyeland <63192606+eyeland@users.noreply.github.com>
2022-07-03 12:25:22 -0400

    chainsaw to random locations

--- NPC/script.js
+++ NPC/script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 200;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -12,24 +12,32 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = 'enemies/enemy3.png'; 
+        this.image.src = 'enemies/enemy4.png'; 
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 218;
-        this.spriteHeight = 177;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
         this.width = this.spriteWidth/2.5;
         this.height = this.spriteHeight/2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 2 + 0.5;
-        //this.curve = Math.random() * 200;
+        this.interval = Math.floor(Math.random() * 200 + 50);
+     
     }
     update(){
-        this.x = canvas.width/2 * Math.cos(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
-        this.y = canvas.height/2 * Math.sin(this.angle * Math.PI/270) + (canvas.height/2 - this.height/2);
-        this.angle += this.angleSpeed;
+        if (gameFrame % this.interval === 0){
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/70;
+        this.y -= dy/70;
+        //this.x = 0;
+        //this.y = 0;
         //animate sprites
         if (this.x + this.width < 0) this.x = canvas.width;
         if (gameFrame % this.flapSpeed === 0) {
